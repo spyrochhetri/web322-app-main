144e70e2d7cc626b19c3ef741a4557fade7648ed
Jahanvi220104 <jahanvij22@gmail.com>
2022-10-21 23:05:39 -0400

    done

--- server.js
+++ server.js
@@ -6,7 +6,7 @@
 * 
 *  Name:Jahanvi Jitendra Randhejiya Student ID:159298215 Date: 3rd october, 2022
 *
-*  Cyclic Web App URL: https://delightful-bear-bathing-suit.cyclic.app/
+*  Cyclic Web App URL: ---------------
 *
 *  GitHub Repository URL: https://github.com/Jahanvi220104/web322-app.git
 *
@@ -14,9 +14,27 @@
  var express = require("express");
  var app = express();
  var path = require("path");
- var data = require("./blog-service");
+ var blogService = require ('./blog-service.js')
+
  var HTTP_PORT = process.env.PORT || 8080;
- 
+
+ //libraries
+const multer = require("multer");
+const cloudinary= require('cloudinary').v2
+const streamifier = require('streamifier')
+
+cloudinary.config({
+  cloud_name: 'dgbjmbkxr',
+  api_key: '764431573664668',
+  api_secret: 'iKg_pblVfzbodEHRFHfZQOwSl1w',
+  secure: true
+});
+
+//upload variable
+const upload = multer();
+
+app.use(express.static('public'));
+
  function onHttpStart() {
    console.log("Express http server listening on: " + HTTP_PORT);
    return new Promise(function (res, req) {
@@ -31,15 +49,18 @@
    });
  }
  app.use(express.static("public"));
+
+
 // GET ABOUT
- app.get("/", function (req, res) {
-   res.sendFile(path.join(__dirname + "/views/about.html"));
- });
- //GET /POST/ADD
- app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname + "/views/addPost.html"));
+app.get("/", (req, res) => {
+  res.redirect('/about');
 });
 
+ app.get("/about", function (req, res) {
+   res.sendFile(path.join(__dirname, "/views/about.html"));
+ });
+
+//BLOGS
  app.get("/blog", function (req, res) {
    data
      .getPublishedPosts()
@@ -51,6 +72,7 @@
      });
  });
  
+ //GET POSTS
  app.get("/posts", function (req, res) {
    data
      .getAllPosts()
@@ -62,6 +84,7 @@
      });
  });
  
+ //GET CATEGORIES
  app.get("/categories", function (req, res) {
    data
      .getCategories()
@@ -73,8 +96,72 @@
      });
  });
  
- app.use(function (req, res) {
-   res.status(404).sendFile(path.join(__dirname, "Page Not Found"));
- });
- 
+ //GET /POST/ADD
+ app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "/views/addPost.html"));
+});
+
  app.listen(HTTP_PORT, onHttpStart)
+
+ // Adding POST routes
+app.post('/posts/add', upload.single("featureImage"), (req, res) => {
+  if(req.file){
+      let streamUpload = (req) => {
+          return new Promise((resolve, reject) => {
+              let stream = cloudinary.uploader.upload_stream(
+                  (error, result) => {
+                      if (result) {
+                          resolve(result);
+                      } else {
+                          reject(error);
+                      }
+                  }
+              );
+  
+              streamifier.createReadStream(req.file.buffer).pipe(stream);
+          });
+      };
+  
+      async function upload(req) {
+          let result = await streamUpload(req);
+          console.log(result);
+          return result;
+      }
+  
+      upload(req).then((uploaded)=>{
+          processPost(uploaded.url);
+      });
+  } else {
+      processPost("");
+  }
+
+  function processPost(imageUrl){
+      req.body.featureImage = imageUrl;
+
+      const postData = {
+          "body": req.body.body,
+          "title": req.body.title,
+          "postDate": new Date().toISOString().split('T')[0],
+          "category": req.body.category,
+          "featureImage": imageUrl,
+          "published": req.body.published,
+      }
+
+      service.addPost(postData).then(data => res.redirect('/posts')).catch(err => res.json(`message: ${err}`));
+  }
+})
+
+//ERROR
+app.use(function (req, res) {
+  res.status(404).sendFile(path.join(__dirname, "Page Not Found"));
+});
+
+
+ blogService.initialize().then(() =>{
+  app.listen(HTTP_PORT, () => {
+      console.log('Express HTTP server is listening to the port', HTTP_PORT)
+  })
+}).catch(() => {
+  console.log('Error: Server not started')
+
+})
